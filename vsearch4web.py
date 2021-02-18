b99e89e0411fd1911590831850ab8cc2c5998cac
oleg <314koval.ok@gmail.com>
2021-02-18 18:27:53 +0200

    add function log_requst and add function view_the_log

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
+    
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
             the_phrase=phrase,
             the_letters=letters,
@@ -20,6 +26,12 @@ def do_search() -> 'html':
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcom to search4letters on the web!')
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
 
 if __name__ == '__main__':
     app.run(debug=True)
