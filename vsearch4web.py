5ca3c6027357bad8b7a11b3eb01c4deac4c4175c
oleg <314koval.ok@gmail.com>
2021-02-28 05:45:33 +0200

    add viewlog.html

--- vsearch4web.py
+++ vsearch4web.py
@@ -28,14 +28,18 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcom to search4letters on the web!')
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
     contents = []
     with open('vsearch.log') as log:
         for line in log:
             contents.append([])
             for item in line.split('|'):
                 contents[-1].append(escape(item))
-    return str(contents)
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                            the_title='View Log',
+                            the_row_titles=titles,
+                            the_data=contents,)
 
 if __name__ == '__main__':
     app.run(debug=True)
